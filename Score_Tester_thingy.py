1836434af60922f899a8a79fbcd5caffd2b33752
No31LE <86942030+No31LE@users.noreply.github.com>
2021-07-06 12:46:25 +0800

    Add files via upload

---
+++ Score_Tester_thingy.py
@@ -0,0 +1,8 @@
+a = float(input('Math Score'))
+b = float(input('English Score'))
+if a >= 90 and b >= 90:
+    print('You got 1 billon dolars')
+elif a <= 89 and b <= 89:
+    print('here is the extra homework')
+elif a <= 89 or b <= 89:
+    print('再加油')
